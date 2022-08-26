658a224ad4f247e76090987c37bb5b10f7202a26
Sai Saing Linn <saisainglinn.me@gmail.com>
2022-08-26 12:23:22 +0630

    Fixed token expired and logout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,13 +1,12 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import { BrowserRouter, useHistory } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { authStore } from '../services'
 import { home } from '../store/action'
 import AppRoute from './../router/router'
 
 export default function Index() {
   const dispatch = useDispatch();
-  const history = useHistory();
   // signout if token expired 
   useEffect(() => {
     if(authStore.getAuth()) {
@@ -15,10 +14,10 @@ export default function Index() {
       let now = +new Date();
       if(now > tokenExpiredTime) {
         dispatch(home.setLogout())
-        history.push('/')
+        window.location.href = "/"
       }
     }
-  }, [dispatch, history])
+  }, [dispatch])
 
   return (
     <BrowserRouter>
